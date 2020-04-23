275dddd492f22deec5153c734b919238bea13807
Steven Groves <stevegroves123@gmail.com>
2020-04-23 11:45:11 +0100

    Updated version with all date information

--- TestWatchFace WatchKit Extension/ContentView.swift
+++ TestWatchFace WatchKit Extension/ContentView.swift
@@ -22,6 +22,7 @@ import SwiftUI
                 )
             }
         }
+
 // create the various variables
     struct ContentView: View {
         @State var degreesPointedTo = 0
@@ -30,28 +31,42 @@ import SwiftUI
         @State var myHour = 0
         @State var myMinute = 0
         @State var mySecond = 0
-        @State var components = Calendar.current.dateComponents([.day, .month, .hour, .minute, .second], from: Date())
+        @State var myWeekday = 0
+        @State var weekDayText = ""
+        @State var myYear = 20
+        @State var components = Calendar.current.dateComponents([.day, .month, .hour, .minute, .second, .weekday, .year], from: Date())
+        @State var scrollAmount = 0.0
+        let colours: [Int: Color] = [
+            0 : Color.red,
+            1 : Color.blue,
+            2 : Color.green,
+            3 : Color.yellow,
+            4 : Color.white,
+            5 : Color.pink,
+            6 : Color.gray,
+            7 : Color.orange
+        ]
  
 // create the tick marks to show the edge of the clock
-        func tick(at tick: Int) -> some View {
-            VStack {
-                Rectangle()
-                    .fill(Color.blue)
-                    .opacity(tick % 5 == 0 ? 1 : 0.4)
-                    .frame(width: 2, height: 8)
-                Spacer()
-                }
-                .frame(width: 150, height: 150, alignment: .center)
-                .rotationEffect(Angle.degrees(Double(tick)/60 * 360))
+    func tick(at tick: Int) -> some View {
+        VStack {
+            Rectangle()
+                .fill(Color.blue)
+                .opacity(tick % 5 == 0 ? 1 : 0.4)
+                .frame(width: 1, height: 10)
+            Spacer()
             }
+            .frame(width: 158, height: 158, alignment: .center)
+            .rotationEffect(Angle.degrees(Double(tick)/60 * 360))
+        }
+        
 // create the second hand
     struct secondHand: Shape {
         var circleRadius: CGFloat = 3
         func path(in rect: CGRect) -> Path {
             Path { p in
                 // create long part of second hand
-                p.move(to: CGPoint(x: rect.midX, y: rect.minY
-                 + 20))
+                p.move(to: CGPoint(x: rect.midX, y: rect.minY + 20))
                 p.addLine(to: CGPoint(x: rect.midX, y: rect.midY - circleRadius))
                 // create center circle
                 p.addEllipse(in: CGRect(center: rect.center, radius: circleRadius))
@@ -64,12 +79,14 @@ import SwiftUI
         
 // timer to update dispaly
     var updateTimer: Timer {
-        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: {
-            _ in self.components = Calendar.current.dateComponents([.day, .month, .hour, .minute, .second], from: Date())
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
+            _ in self.components = Calendar.current.dateComponents([.day, .month, .hour, .minute, .second, .weekday, .year], from: Date())
             self.myHour = self.components.hour ?? 0
             self.mySecond = self.components.second ?? 0
             self.myMonth = self.components.month ?? 0
             self.myDay = self.components.day ?? 0
+            self.myWeekday = self.components.weekday ?? 0
+            self.myYear = self.components.year ?? 0
         })
     }
 
@@ -91,7 +108,7 @@ import SwiftUI
             case 7:
                 return "July"
             case 8:
-                return "August"
+                return "Aug"
             case 9:
                 return "Sept"
             case 10:
@@ -108,14 +125,36 @@ import SwiftUI
 // create lookup for the day - change for an if else statement
         func lookupDay(myDay: Int) -> String {
             switch myDay {
-            case 1:
+            case 1, 21, 31:
                 return "st"
-            case 2:
+            case 2, 22:
                 return "nd"
-            case 3:
+            case 3, 23:
                 return "rd"
             default:
                 return "th"
+            }
+        }
+        
+// create a look up for the month
+        func lookupDayofWeek(myWeekday: Int) -> String {
+            switch myWeekday {
+            case 1:
+                return "Sun"
+            case 2:
+                return "Mon"
+            case 3:
+                return "Tues"
+            case 4:
+                return "Wed"
+            case 5:
+                return "Thur"
+            case 6:
+                return "Fri"
+            case 7:
+                return "Sat"
+            default:
+                return "BlurDay"
             }
         }
     
@@ -125,22 +164,30 @@ import SwiftUI
                 self.tick(at: tickMark)
             }
             secondHand()
-                .stroke(Color.orange, lineWidth: 2)
+                .stroke(lineWidth: 1)
                 .rotationEffect(Angle.degrees(Double(mySecond) * 360 / 60))
             
-            Text("\(self.myDay)\(lookupDay(myDay: self.myDay))")  //need to change the th so it matches the day
-                .offset(x: -60, y:78)
+            Text("\(self.myDay)\(lookupDay(myDay: self.myDay))")
+                .offset(x: 60, y:-78)
             Text("\(lookupMonth(myMonth: myMonth))")
-                .offset(x:60, y:78)
+                .offset(x:-60, y:78)
+            Text("\(lookupDayofWeek(myWeekday: myWeekday))")
+                .offset(x:-60, y:-80)
+            Text("\(myYear.description)")
+                .offset(x: 60, y:75)
             Color.clear
             }
-            .onAppear(perform: {let _ = self.updateTimer})
+        .foregroundColor(colours[Int(scrollAmount)])
+        .focusable(true)
+        .digitalCrownRotation($scrollAmount, from: 0, through: 7, by: 1, sensitivity: .low, isContinuous: true)
+        .onAppear(perform: {let _ = self.updateTimer})
+        .navigationBarTitle("WatchFace")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewLayout(.fixed(width: 170, height: 170))
+            .previewLayout(.fixed(width: 190, height: 190))
     }
 }
